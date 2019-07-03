cea0687f3589b1321fd1759b3fb686dd26a7be37
Rafael Valentim <rmoraesvalentim@gmail.com>
2019-07-03 08:50:30 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars';)
+
+
+
+app.listen(3000, () => {
+    console.log('Server listen on port 3000.')
+});
